cbd4e4d37f0346a18a26e2a4dfbea9f277a6a398
García Sánchez <antonio.garcia-sanchez@capgemini.com>
2021-08-18 18:45:50 +0200

    feat:making better code

--- js/netflixCAP_AGS.js
+++ js/netflixCAP_AGS.js
@@ -14,25 +14,27 @@ window.onload = function () {
     }
 
 
-    function buscar(x) {
+    function buscar(x) {        
 
         switch (x) {
             case 1:
-                num = 1
+                document.getElementById("next").disabled=false;
+                num = 1;                
                 break;
             case 2:
-                num += 1
+                num += 1;
+                document.getElementById("before").disabled=false;
                 break;
             case 3:
-                num -= 1
+                num -= 1;
                 break;
             default:
                 break;
         }
 
-        if (num < 1) {
-            num=1;
-            alert("Ya has llegado al inicio de la búsqueda");
+        if (num <= 1) {                       
+            document.getElementById("before").disabled=true;
+            num = 1; 
         }
 
         let urltitulo = url();
@@ -44,26 +46,55 @@ window.onload = function () {
             })
             .then(function (datos) {
                 let pelis = datos.Search;
-                console.log(datos.totalResults);
-                console.log(Math.ceil(datos.totalResults/10));
+                console.log(datos);
+                console.log(Math.ceil(datos.totalResults / 10));
+                let resultsPage = Math.ceil(datos.totalResults / 10);
+                if (num>=resultsPage) {
+                    document.getElementById("next").disabled=true;
+                }
                 console.log(urlfinal);
-                document.getElementById("resultado").innerHTML = `Se han encontrado ${datos.totalResults} películas con ese criterio de búsqueda`;
+                document.getElementById("resultado").innerHTML = `Se han encontrado ${datos.totalResults} películas que contienen la palabra "${document.getElementById("titulo").value}"`;
                 for (var p in pelis) {
-                    //console.log(pelis[p].Title+" : "+pelis[p].Year+" : "+pelis[p].Released+" : "+pelis[p].Actors+" : "+pelis[p].Genre+" : "+pelis[p].Plot);
-                    document.getElementById("muestra").innerHTML +=
-                        `
-                        <div class="card deck" style="width: 18rem; margin : auto;">
-                            <img class="card-img-top img-thumbnail" src="${pelis[p].Poster}" alt="Card image cap">
-                            <div class="card-body">
-                                <h4 class="card-title text-center">${pelis[p].Title}</h5>
-                                <h5 class="text-center text-secondary">Año: ${pelis[p].Year}</h5>
-                            </div>                        
-                            <ul class="list-group list-group-flush">
-                            <!-- <a href="#" class="btn btn-seconday">Infomación</a>-->
-                            </ul>
-                        </div>                  
-                    <br>
-                    `;
+                    let url2 = "http://www.omdbapi.com/?apikey=a98f434c&t=" + pelis[p].Title;
+                    fetch(url2)
+                        .then(peli => peli.json())
+                        .then(peli => {
+                            document.getElementById("muestra").innerHTML +=
+                                ` 
+                                <div class="col-4">
+                                <div class="row">
+                                    <div class="card deck">
+                                        <img class="card-img-top img-thumbnail" src="${peli.Poster}" type="button" alt="..." data-toggle="modal"
+                                            data-target="#${peli.imdbID}">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="modal fade" id="${peli.imdbID}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">                            
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="card">
+                                             <img src="${peli.Poster}" class="card-img-top" alt="...">
+                                                <div class="card-body">
+                                                    <h3 class="card-title text-center">${peli.Title}</h3>
+                                                    <p class="card-text">${peli.Plot}</p>
+                                                    <p class="card-text"><span class="font-weight-bold">Año:</span> ${peli.Year}</p>                                                    
+                                                    <p class="card-text"><span class="font-weight-bold">Estreno en salas:</span> ${peli.Released}</p>                                                    
+                                                    <p class="card-text"><span class="font-weight-bold">Actores:</span> ${peli.Actors}</p>                                                    
+                                                    <p class="card-text"><span class="font-weight-bold">Genero:</span> ${peli.Genre}</p>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>                            
+                                </div>
+                            </div>
+                            </div>
+                            <br>
+                            `;
+                        });
                 }
             })
             .catch(err => { console.log(err) });
